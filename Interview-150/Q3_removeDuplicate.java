bcdf3d67f79a935b72073c416f6874a8a85a5a32
Akash Sharma <soulakash20002@gmail.com>
2025-03-08 15:18:06 +0530

    remove duplicate from sorted array

---
+++ Interview-150/Q3_removeDuplicate.java
@@ -0,0 +1,15 @@
+public class Q3_removeDuplicate {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int j = 0;
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[j]) {
+                    nums[++j] = nums[i];
+                }
+            }
+
+            return j + 1;
+        }
+    }
+}
