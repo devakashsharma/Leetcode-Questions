956e43a974901d04c0320ac5b5b38c1302b139ca
Akash Sharma <soulakash20002@gmail.com>
2026-01-02 23:56:53 +0530

    update

--- JS-30/JS01_closures/JS3_toBeOrNot.js
+++ JS-30/JS01_closures/JS3_toBeOrNot.js
@@ -1,3 +1,7 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
 var expect = function(val) {
     return {
         toBe: (val2) => {
@@ -10,3 +14,8 @@ var expect = function(val) {
         }
     }
 };
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
